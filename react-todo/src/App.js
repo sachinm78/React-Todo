aed8e1719e057a913cce32ddecac56eb69ee2db1
sachinm78 <sachinm78@gmail.com>
2019-08-22 11:21:35 -0500

    initialized app and created placeholder text on App page

---
+++ react-todo/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div>
+          <input type="checkbox" />
+          <p>Placeholder text here</p>
+          
+          <input type="checkbox" />
+          <p>Placeholder text here</p>
+          
+          <input type="checkbox" />
+          <p>Placeholder text here</p>
+      
+          <input type="checkbox" />
+          <p>Placeholder text here</p>
+      </div>
+  )
+}
+
+export default App
